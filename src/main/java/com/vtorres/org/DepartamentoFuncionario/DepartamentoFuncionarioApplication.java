023c17d6bb3f41e67efa1620fba3414b60dd9e1b
Victor Torres <clerossephirot@gmail.com>
2024-05-30 15:17:20 -0300

    Inicio do Projeto

---
+++ src/main/java/com/vtorres/org/DepartamentoFuncionario/DepartamentoFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.vtorres.org.DepartamentoFuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentoFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentoFuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtorres/org/DepartamentoFuncionario/DepartamentoFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtorres.org.DepartamentoFuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentoFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
